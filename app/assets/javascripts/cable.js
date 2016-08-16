ccbb9c5960be75de5cae740229cae1c09004ff89
RB <ryanblair1993@gmail.com>
2016-08-16 14:44:26 -0700

    added color field, price to product table + added images

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,4 +1,4 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
+m// Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the rails generate channel command.
 //
 //= require action_cable
