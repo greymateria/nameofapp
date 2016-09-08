67b28a7b4c9aa9fe896db42162afb70d3dfe3252
RB <ryanblair1993@gmail.com>
2016-09-08 14:18:39 -0700

    five branch test

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/shop'
-
-  get 'welcome/about'
-
-  get 'welcome/contact'
 
   resources :products
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-	#  get 'static_pages/index'
+	#  get 'static_pages/index' - root is the home page
   root 'static_pages#landing_page'
 
   # added in 4.3 for app/views/orders
