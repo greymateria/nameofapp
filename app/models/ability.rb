7c5f3e8da650e0e0f148b1b277f92e80bdbabf2d
RB <ryanblair1993@gmail.com>
2016-09-27 09:12:22 -0700

    removed admin from ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
-        can :manage, :all
-    else
         can :manage, User, id: user.id
 
     # Define abilities for the passed in user here. For example:
