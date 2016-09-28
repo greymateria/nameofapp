5cfb15919b77c9d02c1fb99f6ad86dbc0cb6477e
RB <ryanblair1993@gmail.com>
2016-09-27 18:51:29 -0700

    moved Images from /assets and into public folder to test Raty stars on Heroku

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,9 @@
 // Raty plugin
 $(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
+	// raty images path changed from /assets to /images/ratyrate:
+  $('.rating').raty( { path: '/images/ratyrate', scoreName: 'comment[rating]' });
+  // raty images path changed from /assets to /images/ratyrate:
+  $('.rated').raty({ path: '/images/ratyrate',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
