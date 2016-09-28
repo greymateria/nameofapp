ef9c32849ce419f2ebc64c049c140b59389ae46c
RB <ryanblair1993@gmail.com>
2016-09-27 20:53:01 -0700

    raty heroku again

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,15 @@
-// Raty plugin
-$(document).on('turbolinks:load', function(){
-	// raty images path changed from /assets to /images/ratyrate:
-  $('.rating').raty( { path: '/images/ratyrate', scoreName: 'comment[rating]' });
-  // raty images path changed from /assets to /images/ratyrate:
-  $('.rated').raty({ path: '/images/ratyrate',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+var refreshRating = function() {
+	$('.rating').raty( { path: '/images/ratyrate', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/images/ratyrate',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
+};
+
+$(document).on('turbolinks:load', function() {
+	refreshRating();
+
+	$('.img-zoom').elevateZoom();
 });
