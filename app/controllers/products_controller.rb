2e8abf6823ef4c557b7ec9edc7fcf86f80421dce
RB <ryanblair1993@gmail.com>
2016-10-01 10:59:39 -0700

    6.7 byebug install and test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,12 +11,14 @@ class ProductsController < ApplicationController
     else
       @products = Product.all
     end
+
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+
   end
 
   # GET /products/new
