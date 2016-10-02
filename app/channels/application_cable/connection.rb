cb8a1dd2bed6fb44c03aa3b9d8d742e3c2513bce
RB <ryanblair1993@gmail.com>
2016-10-01 21:57:00 -0700

    6.14

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,24 @@
 # used to run any sort of code before the connection between a user and the server is established
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	identified_by :current_user
+
+  	def connect
+
+    end
+
+    def disconnect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+      def find_verified_user
+        if verified_user = request.env['warden'].user
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
